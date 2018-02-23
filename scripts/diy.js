586be98b286cbdd2319e6fab458279536088147b
BabuRaj <BabuRaj@Matrix-PC>
2018-02-24 00:34:34 +0530

    ok

--- scripts/diy.js
+++ scripts/diy.js
@@ -6,3 +6,10 @@ function loadPage(pageNumber){
 	$( "#mainContent" ).load( "html/"+pageNumber );
 	
 }
+$('#pagination-demo').twbsPagination({
+    totalPages: 3 ,
+    visiblePages: 3,
+    onPageClick: function (event, page) {
+        loadPage(page);
+    }
+});
