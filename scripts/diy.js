3af7cc3607aa4c478d059a39e2c141e4af97f810
BabuRaj <BabuRaj@Matrix-PC>
2018-02-24 23:54:41 +0530

    k

--- scripts/diy.js
+++ scripts/diy.js
@@ -3,13 +3,14 @@
  */
 function loadPage(pageNumber){
 	pageNumber = pageNumber+".html";
-	$( "#mainContent" ).load( "html/"+pageNumber );
+	$( "#mainContent" ).load( "view/"+pageNumber );
 	$('html, body').animate({ scrollTop: 0 }, 'fast');
 	
 }
 $('#pagination-demo').twbsPagination({
-    totalPages: 3 ,
+    totalPages: 3,
     visiblePages: 3,
+    hideOnlyOnePage:true,
     onPageClick: function (event, page) {
         loadPage(page);
     }
