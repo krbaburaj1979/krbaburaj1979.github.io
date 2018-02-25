0897e5ae6f67a8b80f11347be4573cacfeb897a4
BabuRaj <BabuRaj@Matrix-PC>
2018-02-26 00:27:57 +0530

    k

--- scripts/diy.js
+++ scripts/diy.js
@@ -1,17 +1,43 @@
-/**
- * 
- */
-function loadPage(pageNumber){
-	pageNumber = pageNumber+".html";
-	$( "#mainContent" ).load( "view/"+pageNumber );
-	$('html, body').animate({ scrollTop: 0 }, 'fast');
-	
+var scrollInAction = false;
+var index = 1;
+var maxPage = 3;
+
+$(window).scroll(
+		function() {
+			if ($(window).scrollTop() >= ($(document).height()
+					- $(window).height() - 200)) {
+
+				if (scrollInAction) {
+					return false;
+				} else {
+					scrollInAction = true;
+					// do stuff // load of more content here
+					loadPage();
+
+					// scrollInAction=false; // TODO: PUT this
+					// scrollInAction=false after ajax loading is completed.
+				}
+			}
+		});
+
+function loadPage() {
+	$.ajax({
+		url : "view/" + index + ".html",
+		type : "post",
+		beforeSend : function(xhr) {
+
+		},
+		success : function(data) {
+			$("#mainContent").append(data);
+		}
+	});
+
+	index = index + 1;
+
+	if (index > maxPage) 
+	{
+		scrollInAction = true;
+	} else {
+		scrollInAction = false;
+	}
 }
-$('#pagination-demo').twbsPagination({
-    totalPages: 3,
-    visiblePages: 3,
-    hideOnlyOnePage:true,
-    onPageClick: function (event, page) {
-        loadPage(page);
-    }
-});
