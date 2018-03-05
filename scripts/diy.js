08d7467cfa322edac17dc47fafc1e5eef8535a7a
baburaj <krbaburaj@gmail.com>
2018-03-05 17:11:56 +0530

    k

--- scripts/diy.js
+++ scripts/diy.js
@@ -3,43 +3,38 @@ var scrollActionLock = false;
 var index = 1;
 var maxPage = 3;
 
-/*$(window).scroll(
-		function() {
-			if(index==2) {
-				alert($(window).scrollTop() +"   "+$(document).height()+"        "+$(window).height());
-			}
-			if ($(window).scrollTop() >= ($(document).height()
-					- $(window).height() - 1000)) {
-
-				if (scrollInAction) {
-					return false;
-				} else {
-					scrollInAction = true;
-					// do stuff // load of more content here
-					loadPage();
-
-					// scrollInAction=false; // TODO: PUT this
-					// scrollInAction=false after ajax loading is completed.
-				}
-			}
-		});*/
+/*
+ * $(window).scroll( function() { if(index==2) { alert($(window).scrollTop() +"
+ * "+$(document).height()+" "+$(window).height()); } if ($(window).scrollTop() >=
+ * ($(document).height() - $(window).height() - 1000)) {
+ * 
+ * if (scrollInAction) { return false; } else { scrollInAction = true; // do
+ * stuff // load of more content here loadPage(); // scrollInAction=false; //
+ * TODO: PUT this // scrollInAction=false after ajax loading is completed. } }
+ * });
+ */
 
 function loadPage() {
-	if(!scrollActionLock) {
+	if (!scrollActionLock) {
+		$("#wait").css("display", "block");
+
 		$.ajax({
 			url : "./view/" + index + ".html",
 			type : "get",
-			dataType: "html",
-			contentType:"html",
+			dataType : "html",
+			contentType : "html",
+			complete : function(data) {
+				
+			},
 			success : function(data) {
-				$("#mainContent").append(data);
+				$("#mainContent").append(data);				
 			}
+		
 		});
-	
+
 		index = index + 1;
-	
-		if (index > maxPage) 
-		{
+
+		if (index > maxPage) {
 			scrollInAction = true;
 		} else {
 			scrollInAction = false;
@@ -47,40 +42,69 @@ function loadPage() {
 	}
 }
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)), sURLVariables = sPageURL
+			.split('&'), sParameterName, i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
-    }
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
+	}
 };
 
 function loadSubPage() {
 	var subpage = getUrlParameter("subPage");
-	if(subpage) {
+	if (subpage) {
+		$("#wait").css("display", "block");
 		var heading = getUrlParameter("heading");
 		var date = getUrlParameter("date");
-		$('#mainContent').load("view/subpage/"+subpage+".html");
-		$('#headingId').html(heading);
 		
+		
+		
+		$.ajax({
+			url : "view/subpage/" + subpage + ".html",
+			type : "get",
+			dataType : "html",
+			contentType : "html",
+			complete : function(data) {
+				$("#wait").css("display", "none");
+			},
+			success : function(data) {
+				$("#mainContent").append(data);	
+				$('#headingId').html(heading);
+
+				$('#subPageDateContainer').html(date);
+				$('html, body').animate({
+					scrollTop : $("#headingId").offset().top
+				}, 1000);
+				scrollActionLock = true;
+				$("#headingId").css("background", "#FFF8DC");
+				$("#pagination").css("display", "none");
+			}
+		
+		});
+		
+		
+		
+		
+		
+		
+		$('#mainContent').load("view/subpage/" + subpage + ".html");
+		$('#headingId').html(heading);
+
 		$('#subPageDateContainer').html(date);
 		$('html, body').animate({
-	        scrollTop: $("#headingId").offset().top
-	    }, 1000);
+			scrollTop : $("#headingId").offset().top
+		}, 1000);
 		scrollActionLock = true;
-		$("#headingId").css( "background", "#FFF8DC" );
-		$("#pagination").css("display","none");
+		$("#headingId").css("background", "#FFF8DC");
+		$("#pagination").css("display", "none");
 		return true;
-	}
-	else {
+	} else {
 		scrollActionLock = false;
-		return false; 
+		return false;
 	}
 }
 function showAboutMe() {
@@ -89,4 +113,3 @@ function showAboutMe() {
 function showContactMe() {
 	$('#contactModalId').modal();
 }
-
