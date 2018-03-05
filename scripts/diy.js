24b6b3aa6e40c983191baf9ad1ab586c038b9c1a
baburaj <krbaburaj@gmail.com>
2018-03-05 17:35:19 +0530

    k

--- scripts/diy.js
+++ scripts/diy.js
@@ -1,45 +1,19 @@
-var scrollInAction = false;
-var scrollActionLock = false;
-var index = 1;
-var maxPage = 3;
-
-/*
- * $(window).scroll( function() { if(index==2) { alert($(window).scrollTop() +"
- * "+$(document).height()+" "+$(window).height()); } if ($(window).scrollTop() >=
- * ($(document).height() - $(window).height() - 1000)) {
- * 
- * if (scrollInAction) { return false; } else { scrollInAction = true; // do
- * stuff // load of more content here loadPage(); // scrollInAction=false; //
- * TODO: PUT this // scrollInAction=false after ajax loading is completed. } }
- * });
- */
-
 function loadPage() {
-	if (!scrollActionLock) {
-		$("#wait").css("display", "block");
+	$("#wait").css("display", "block");
 
-		$.ajax({
-			url : "./view/" + index + ".html",
-			type : "get",
-			dataType : "html",
-			contentType : "html",
-			complete : function(data) {
-				
-			},
-			success : function(data) {
-				$("#mainContent").append(data);				
-			}
-		
-		});
+	$.ajax({
+		url : "./view/" + 1 + ".html",
+		type : "get",
+		dataType : "html",
+		contentType : "html",
+		complete : function(data) {
 
-		index = index + 1;
-
-		if (index > maxPage) {
-			scrollInAction = true;
-		} else {
-			scrollInAction = false;
+		},
+		success : function(data) {
+			$("#mainContent").append(data);
 		}
-	}
+
+	});
 }
 var getUrlParameter = function getUrlParameter(sParam) {
 	var sPageURL = decodeURIComponent(window.location.search.substring(1)), sURLVariables = sPageURL
@@ -60,50 +34,31 @@ function loadSubPage() {
 		$("#wait").css("display", "block");
 		var heading = getUrlParameter("heading");
 		var date = getUrlParameter("date");
-		
-		
-		
+
 		$.ajax({
 			url : "view/subpage/" + subpage + ".html",
 			type : "get",
 			dataType : "html",
 			contentType : "html",
 			complete : function(data) {
-				$("#wait").css("display", "none");
+
 			},
 			success : function(data) {
-				$("#mainContent").append(data);	
+				$("#mainContent").append(data);
 				$('#headingId').html(heading);
 
 				$('#subPageDateContainer').html(date);
 				$('html, body').animate({
 					scrollTop : $("#headingId").offset().top
 				}, 1000);
-				scrollActionLock = true;
 				$("#headingId").css("background", "#FFF8DC");
 				$("#pagination").css("display", "none");
 			}
-		
-		});
-		
-		
-		
-		
-		
-		
-		$('#mainContent').load("view/subpage/" + subpage + ".html");
-		$('#headingId').html(heading);
 
-		$('#subPageDateContainer').html(date);
-		$('html, body').animate({
-			scrollTop : $("#headingId").offset().top
-		}, 1000);
-		scrollActionLock = true;
-		$("#headingId").css("background", "#FFF8DC");
-		$("#pagination").css("display", "none");
+		});
+
 		return true;
 	} else {
-		scrollActionLock = false;
 		return false;
 	}
 }
