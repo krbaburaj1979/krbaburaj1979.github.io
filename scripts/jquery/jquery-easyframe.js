8f4258d3d89ee418996c817b892b5c07ff4522dd
baburaj <baburaj@stxapp3.stabilixindia.biz>
2018-03-07 20:16:31 +0530

    k

---
+++ scripts/jquery/jquery-easyframe.js
@@ -0,0 +1,43 @@
+/*! jquery-easyframe - v0.3.0 */
+(function($) {
+	$.fn.easyframe = function(options, callback) {
+		callback = $.isFunction(options) ? options : callback;
+		var settings = $.extend({
+			url : null,
+			attribute : "data-src",
+			timeout : 0
+		}, options), iframe = $(this), currentFramesLoaded = 0;
+		setTimeout(
+				function() {
+					iframe
+							.each(function() {
+								var currentElement = $(this);
+								var iframeURL = settings.url == null ? currentElement
+										.attr(settings.attribute)
+										: settings.url;
+								if (typeof iframeURL !== "undefined"
+										&& typeof iframeURL !== "null"
+										&& $.trim(iframeURL) !== "") {
+									$(this).attr("src", iframeURL);
+									if ($.isFunction(callback)) {
+										currentElement
+												.on(
+														"load",
+														function() {
+															currentFramesLoaded++;
+															if (currentFramesLoaded == iframe.length) {
+																callback();
+															}
+														});
+									}
+								} else {
+									console
+											.error("EasyFrame: You must set an url from the options or from the "
+													+ settings.attribute
+													+ " attribute.");
+								}
+							});
+				}, settings.timeout);
+
+	};
+})(jQuery);
